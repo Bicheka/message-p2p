582f7d5dffed3e54ec9d106ae69571dc3eed7825
David Martinez Gil <davidmartinezgilp@gmail.com>
2024-10-05 01:44:13 -0400

    added support for tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,18 +12,41 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div className="bg-gray-900 p-10 h-screen text-white text-center">
+      <h1 className="text-4xl py-5">Welcome to Tauri!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+      <div className="flex justify-center">
+        <a href="https://vitejs.dev" target="_blank" className="m-8">
+          <div className="relative group inline-block">
+            <div className="absolute inset-0 transform scale-125 bg-pink-500 opacity-0 group-hover:opacity-50 blur-2xl rounded-full transition duration-300 ease-in-out"></div>
+            <img
+              src="/vite.svg"
+              className="relative w-28 h-28 z-10"
+              alt="Vite logo"
+            />
+          </div>
         </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+
+        <a href="https://tauri.app" target="_blank" className="m-8">
+          <div className="relative group inline-block">
+            <div className="absolute inset-0 transform scale-125 bg-pink-500 opacity-0 group-hover:opacity-50 blur-2xl rounded-full transition duration-300 ease-in-out"></div>
+            <img
+              src="/tauri.svg"
+              className="relative w-28 h-28 z-10"
+              alt="Tauri logo"
+            />
+          </div>
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <a href="https://reactjs.org" target="_blank" className="m-8">
+          <div className="relative group inline-block">
+            <div className="absolute inset-0 transform scale-125 bg-pink-500 opacity-0 group-hover:opacity-50 blur-2xl rounded-full transition duration-300 ease-in-out"></div>
+            <img
+              src={reactLogo}
+              className="relative w-28 h-28 z-10"
+              alt="React logo"
+            />
+          </div>
         </a>
       </div>
 
